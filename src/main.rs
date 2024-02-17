1f0a91fbf4adc3e545a8d6622dd61f090fb868b4
misarb <lahcen.boulbalah@gmail.com>
2024-02-17 19:18:56 +0100

    resolve test issue

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,6 @@
-use actix_web::{get, post, App, HttpRequest, HttpResponse, HttpServer, Responder};
+use splinker::run;
 
-#[get("/")]
-async fn hello() -> impl Responder {
-    HttpResponse::Ok().body("Hello Reg")
-}
-
-#[get("/test")]
-async fn tesReq() -> impl Responder {
-    HttpResponse::Ok().body("req responder test")
-}
-
-#[actix_web::main]
-async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().service(hello).service(tesReq))
-        .bind(("127.0.0.1", 8000))?
-        .run()
-        .await
+#[tokio::main]
+async fn main() -> Result<(), std::io::Error> {
+    run()?.await
 }
